63682bada0be3eec4c38f7516e1935a2ddf8b76d
Konstantinov Maksim <75157488+Maxsman-Main@users.noreply.github.com>
2022-07-26 16:02:43 +1000

    Update InputHandler.cs

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -6,6 +6,7 @@ namespace Input
 {
     public class InputHandler : MonoBehaviour
     {
+        //todo new class, which bind buttons for buttonsConfig and link its with InputHandler(I guess good idea use pattern Mediator?)
         [SerializeField] private MovableActor _movableActor;
         
         private ButtonsConfig _buttons;
