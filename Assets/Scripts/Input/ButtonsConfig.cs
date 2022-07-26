133cad5d8499e61a791ae7672e11ebf5c02be634
Maxsman <dekxirus@gmail.com>
2022-07-26 15:34:25 +1000

    ButtonsConfig and InputHandler logic were added

---
+++ Assets/Scripts/Input/ButtonsConfig.cs
@@ -0,0 +1,11 @@
+using System;
+using Command;
+
+namespace Input
+{
+    public class ButtonsConfig
+    {
+        public ICommand ButtonA { get; set; }
+        public ICommand ButtonD { get; set; }
+    }
+}
--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -1,19 +1,30 @@
+using System;
+using Command;
 using UnityEngine;
 
 namespace Input
 {
     public class InputHandler : MonoBehaviour
     {
+        private ButtonsConfig _buttons;
+        
+        private void Start()
+        {
+            _buttons = new ButtonsConfig();
+            _buttons.ButtonA = new MovingLeft();
+            _buttons.ButtonD = new MovingRight();
+        }
+
         private void FixedUpdate()
         {
             if (UnityEngine.Input.GetKey(KeyCode.A))
             {
-                
+                _buttons.ButtonA.Execute();
             }
 
             if (UnityEngine.Input.GetKey(KeyCode.D))
             {
-                
+                _buttons.ButtonD.Execute();
             }
         }
     }
