e0383c4430d96052f8abed66ee69aa7f2fa60dfd
Maxsman <dekxirus@gmail.com>
2022-07-26 15:13:35 +1000

    Interface for Commands, Commands MovingRight and Moving Left without logic, InputHandler were created

---
+++ Assets/Scripts/Input/InputHandler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Input
+{
+    public class InputHandler : MonoBehaviour
+    {
+        private void FixedUpdate()
+        {
+            if (UnityEngine.Input.GetKey(KeyCode.A))
+            {
+                
+            }
+
+            if (UnityEngine.Input.GetKey(KeyCode.D))
+            {
+                
+            }
+        }
+    }
+}
