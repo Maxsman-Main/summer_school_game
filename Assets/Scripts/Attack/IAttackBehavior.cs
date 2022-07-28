aefcc4e77eaab0b681eb170c6a7465541f0df99d
Maxsman <dekxirus@gmail.com>
2022-07-28 23:09:19 +1000

    Interfaces for Attack and Items were added

---
+++ Assets/Scripts/Attack/IAttackBehavior.cs
@@ -0,0 +1,7 @@
+namespace Attack
+{
+    public interface IAttackBehavior
+    {
+        public void Attack();
+    }
+}
---
+++ Assets/Scripts/Items/IItem.cs
@@ -0,0 +1,7 @@
+namespace Items
+{
+    public interface IItem
+    {
+        public void Use();
+    }
+}
